3df2f5eb5bf80ab6bdc044fa5d066cdfced235a4
Carlos Henrique Hollerveger <chhollerveger@gmail.com>
2023-10-24 15:59:01 -0300

    refactor(): added validation

--- src/application/input-adapters/controllers/orders/create-order-controller.ts
+++ src/application/input-adapters/controllers/orders/create-order-controller.ts
@@ -4,28 +4,31 @@ import { CreateOrderUseCase } from '../../../../domain/ports/use-cases/orders/cr
 import { z } from 'zod'
 
 export class CreateOrderController implements Controller {
-  constructor(private readonly createOrderUseCase: CreateOrderUseCase) {}
+  constructor(private readonly createOrderUseCase: CreateOrderUseCase) { }
 
-  public async execute(
-    request: FastifyRequest,
-    reply: FastifyReply,
-  ): Promise<any> {
-    const bodySchema = z.object({
-      items: z
-        .array(
-          z.object({
-            itemId: z.number(),
-            quantity: z.number(),
-          }),
-        )
-        .nonempty(),
-      clientId: z.number().optional(),
-    })
-    const { items, clientId } = bodySchema.parse(request.body)
-    const order = await this.createOrderUseCase.execute(items, clientId)
+  public async execute(request: FastifyRequest, reply: FastifyReply): Promise<any> {
+    const result = this.validate(request.body)
+    if (!result.success) {
+      return reply.status(400).send({
+        message: 'Validation error!',
+        issues: result.error.issues,
+      })
+    }
+    const order = await this.createOrderUseCase.execute(result.data.items, result.data.clientId);
     return reply.status(201).send({
-      message: 'Pedido criado com sucesso!',
+      message: 'Order created successfully!',
       orderId: order.id,
     })
+  }
+
+  private validate(body: FastifyRequest['body']) {
+    const schema = z.object({
+      items: z.array(z.object({
+        itemId: z.number(),
+        quantity: z.number(),
+      })).nonempty(),
+      clientId: z.number().optional(),
+    })
+    return schema.safeParse(body)
   }
 }
