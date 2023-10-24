bc7374101ef3a43fbcc537490ea728b38e0e07bd
Carlos Henrique Hollerveger <chhollerveger@gmail.com>
2023-10-24 15:58:47 -0300

    refactor(): adjustment reoute

--- src/application/input-adapters/routes/order/create-order-route.ts
+++ src/application/input-adapters/routes/order/create-order-route.ts
@@ -4,17 +4,12 @@ import { OrderRepositoryImpl } from '../../../output-adapters/repositories/order
 import { CreateOrderUseCaseImpl } from '../../../use-cases/order/create-order-use-case'
 import { CreateOrderController } from '../../controllers/orders/create-order-controller'
 
-export function createOrderRoute(
-  fastify: FastifyInstance,
-  options: any,
-  done: () => void,
-): void {
-  const orm = SingletonOrmDatabaseAdapter.getInstance()
-  const repository = new OrderRepositoryImpl(orm.database)
-  const usecase = new CreateOrderUseCaseImpl(repository)
-  const controller = new CreateOrderController(usecase)
-
-  fastify.post('/orders', controller.execute.bind(controller))
-
-  done()
+export const createOrderRoute = async (fastify: FastifyInstance) => {
+  fastify.post('/orders', async (request: FastifyRequest, reply: FastifyReply) => {
+    const orm = SingletonOrmDatabaseAdapter.getInstance()
+    const repository = new OrderRepositoryImpl(orm.database)
+    const usecase = new CreateOrderUseCaseImpl(repository)
+    const controller = new CreateOrderController(usecase)
+    await controller.execute(request, reply);
+  })
 }
